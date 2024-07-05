fb78d59f11e9fe0bee73871f42e11e2a662d7e36
juci11 <160618137+juci11@users.noreply.github.com>
2024-07-04 21:16:03 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,99 +1,196 @@
 let questions = [
+
     {
+
         numb: 1,
+
         question: "Coloque o acento na palavra correta: Palavra: 'bebe'",
+
         answer: "bebê",
+
         options: [
+
             "bêbe",
+
             "bebê"
+
         ]
+
     },
+
     {
+
         numb: 2,
+
         question: "Escreva a palavra corretamente acentuada: Palavra: 'pais'",
+
         answer: "país",
+
         options: [
+
             "páis",
+
             "país"
+
         ]
+
     },
+
     {
+
         numb: 3,
+
         question: "Identifique o erro de acentuação: Palavra: 'maquina'",
+
         answer: "máquina",
+
         options: [
+
             "máquina",
+
             "maquína"
+
         ]
+
     },
+
     {
+
         numb: 4,
+
         question: "Complete a palavra com o acento correto: Palavra: 'acoes'",
+
         answer: "ações",
+
         options: [
+
             "acoés",
+
             "ações"
+
         ]
+
     },
+
     {
+
         numb: 5,
+
         question: "Reescreva a palavra com a acentuação adequada: Palavra: 'relogio'",
+
         answer: "relógio",
+
         options: [
+
             "relógio",
+
             "rélogiô"
+
         ]
+
     },
+
     {
+
         numb: 6,
+
         question: "Marque o local onde deve ser colocado o acento: Palavra: 'exercito'",
+
         answer: "exército",
+
         options: [
+
             "exercíto",
+
             "exército"
+
         ]
+
     },
+
     {
+
         numb: 7,
-        question: "Escreva a palavra acentuada corretamente: Palavra: 'mapa'",
-        answer: "mapá",
+
+        question: "Escreva a palavra acentuada corretamente: Palavra: 'virus'",
+
+        answer: "vírus",
+
         options: [
-            "mápa",
-            "mapá"
+
+            "vírus",
+
+            "virús"
+
         ]
+
     },
+
     {
+
         numb: 8,
+
         question: "Indique a posição correta do acento: Palavra: 'rapida'",
+
         answer: "rápida",
+
         options: [
+
             "rapída",
+
             "rápida"
+
         ]
+
     },
+
   
+
     {
+
         numb: 9,
+
         question: "Marque o local onde deve ser colocado o acento: Palavra: 'agua'",
+
         answer: "água",
+
         options: [
+
             "aguá",
+
             "água",
+
            
+
         ]
+
     },
+
     {
+
         numb: 10,
-        question: "Marque o local onde deve ser colocado o acento: Palavra: 'lingua'",
-        answer: "língua",
+
+        question: "Marque o local onde deve ser colocado o acento: Palavra: "Cracha",
+
+        answer: " Crachá",
+
         options: [
-            "língua",
-            "lingua",
+
+            "Crachá",
+
+            "Crácha",
+
             
+
         ]
+
     },
+
    
+
     
+
         ]
+
     
 
